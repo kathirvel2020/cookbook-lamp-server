7a2b3ced3050ea890352fb588e5529c69f1e827c
Bill Hunt <bill@krues8dr.com>
2016-06-07 12:22:13 -0400

    Updating recipes for database creation.

--- recipes/configure-sites.rb
+++ recipes/configure-sites.rb
@@ -1,4 +1,8 @@
 include_recipe 'chef-vault::default'
+chef_gem 'chef-helpers' do
+  compile_time true
+end
+require 'chef-helpers'
 
 # Create a list of the users. Apparently you can't just iterate over the data
 # bag's contents directly.
@@ -6,38 +10,97 @@ sites = data_bag('sites').select do |key|
   !key.end_with?('_keys')
 end
 
+# sites = ['chicago'] #DEBUG
+
 apacheOwner = 'deploy'
 apacheGroup = 'staff'
 
-# Make a directory for vhosts
+# Make a directory for vhosts.
 directory "/var/www/vhosts" do
-  owner  apacheOwner
-  group  apacheGroup
-  mode   '0775'
+  owner apacheOwner
+  group apacheGroup
+  mode '0775'
   action :create
+end
+
+# Read our root database password.
+ruby_block 'get_database_password' do
+  only_if { File.exist?('/root/.my.cnf') }
+  block do
+    f = File.open('/root/.my.cnf', 'r')
+    f.each_line do |line|
+      line.match(/^password=(.*)$/) {|m|
+        node.default['mariadb']['server_root_password'] = m[1]
+        break;
+      }
+    end
+  end
 end
 
 sites.each do |siteName|
   # Get the users' real data from the vault.
   fullSite = chef_vault_item('sites', siteName)
 
-  # Make a directory for the site
-  directory "/var/www/vhosts/#{siteName}" do
-    owner  apacheOwner
-    group  apacheGroup
-    mode   '0775'
-    action :create
-  end
+  # fullSite = { #DEBUG
+  #   'database-name' => 'chicago',
+  #   'database-password' => 'ogjvNJ8KBZ4e',
+  #   'database-username' => 'chicago',
+  #   'dev-url' => 'devchicago.mymadison.io',
+  #   'id' => 'madison-chicago',
+  #   'name' =>  'Madison Chicago',
+  #   'shortname' => 'chicago',
+  #   'url' => 'chicago.mymadison.io',
+  #   'type' => 'madison',
+  #   'servers' => ['Madison-Dev-1','default-ubuntu-1404']
+  # }
 
-  siteTemplate = 'apache/standard.conf.erb'
-  if 'madison' == fullSite['type']
-    siteTemplate = 'apache/madison.conf.erb'
-  end
+  # If the node is one this site belongs to, set it up on the box.
+  if fullSite['servers'].include? node.name
 
-  web_app siteName do
-    server_name fullSite['url']
-    docroot     "/var/www/vhosts/#{siteName}"
-    template    siteTemplate
+    # Apache setup. All sites get Apache.
+
+    # Make a directory for the site.
+    directory "/var/www/vhosts/#{siteName}" do
+      owner apacheOwner
+      group apacheGroup
+      mode '0775'
+      action :create
+    end
+
+    templateExists = has_source?("apache/#{fullSite['type']}.conf.erb",
+      :templates, 'lamp-server')
+
+    if templateExists
+      siteTemplate = "apache/#{fullSite['type']}.conf.erb"
+      puts "Apache Template: Using #{fullSite['type']}"
+    else
+      siteTemplate = 'apache/standard.conf.erb'
+      puts "Apache Template: Using default"
+    end
+
+    web_app siteName do
+      server_name fullSite['url']
+      server_aliases fullSite['aliases']
+      docroot "/var/www/vhosts/#{siteName}"
+      template siteTemplate
+    end
+
+    # MySQL setup. Not all sites get MySQL.
+    if ['madison', 'wordpress'].include? fullSite['type']
+      # mysql2_chef_gem should have been installed in the install-database step.
+      mysql_database siteName do
+        connection(
+          :host     => '127.0.0.1',
+          :username => 'root',
+          :password => node.default['mariadb']['server_root_password']
+        )
+        action :create
+      end
+
+      # TODO Setup user.
+      # TODO Grant access to database.
+    end
+
   end
 
 end
--- recipes/install-database.rb
+++ recipes/install-database.rb
@@ -1,9 +1,6 @@
 # Configure MariaDB.
 
-db_password = random_password
-
 node.default['mariadb']['use_default_repository'] = true
-node.default['mariadb']['server_root_password'] = db_password
 
 # Enable these to overwrite the existing root password.
 # node.default['mariadb']['allow_root_pass_change'] = true
@@ -11,12 +8,22 @@ node.default['mariadb']['server_root_password'] = db_password
 include_recipe "ca-certificates::default"
 
 include_recipe "mariadb::default"
+include_recipe "mariadb::client"
+
+root_db_password = random_password
+node.default['mariadb']['server_root_password'] = root_db_password
 
 template '/root/.my.cnf' do
   # Change the action to :create to overwrite the root password.
   action :create_if_missing
   source 'mariadb/.my.cnf.erb'
-  owner  'root'
-  group  'root'
-  mode   '0600'
+  owner 'root'
+  group 'root'
+  mode '0600'
+end
+
+# We use the mysql2_chef_gem to create databases later.
+mysql2_chef_gem 'default' do
+  provider Chef::Provider::Mysql2ChefGem::Mariadb
+  action :install
 end
