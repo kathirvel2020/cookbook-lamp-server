f3eb856890226960444f458975bfbe0d3713e86f
Bill Hunt <bill@krues8dr.com>
2016-06-20 17:01:21 -0400

    Swapping out the stock php cookbook for a custom one.

--- Berksfile
+++ Berksfile
@@ -1,3 +1,5 @@
 source "https://supermarket.chef.io"
 
 metadata
+
+cookbook 'php', git: 'git@github.com:opengovfoundation/cookbook-php.git'
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,6 +6,6 @@
 # Setup webserver.
 
 include_recipe 'apt'
-include_recipe 'lamp-server::install-webserver'
 include_recipe 'lamp-server::install-database'
+include_recipe 'lamp-server::install-webserver'
 include_recipe 'lamp-server::configure-sites'
