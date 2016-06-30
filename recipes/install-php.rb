9dbb01b3476315685353727ce4221580de1691b1
Seth Etter <sethetter@gmail.com>
2016-06-29 21:43:42 -0500

    Make sure `pear` is installed for PHP

--- recipes/install-php.rb
+++ recipes/install-php.rb
@@ -1,9 +1,14 @@
+package "php-pear" do
+  action :install
+end
+
 # Install PHP
 node.default['php']['version'] = '5.6.22'
 node.default['php']['install_method'] = 'source'
 node.default['php']['checksum'] = '4ce0f58c3842332c4e3bb2ec1c936c6817294273abaa37ea0ef7ca2a68cf9b21'
 
 puts "PHP Packages"
-puts node.default['php']['packages'].push('php5-mcrypt')
+node.default['php']['packages'].push('php5-mcrypt')
+node.default['php']['packages'].push('php5-mysql')
 
 include_recipe 'php'
